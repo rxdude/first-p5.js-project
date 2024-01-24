e575013da2dab6b3d335737f3c436f7426deaf85
rxdude <132919715+rxdude@users.noreply.github.com>
2024-01-24 20:36:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+
+}
+
+function setup(){
+    canvas = createCanvas (640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0 , 0 , 640 , 480 );
+
+}
+
+function take_snapshot() {
+    save('image.png');
+}
